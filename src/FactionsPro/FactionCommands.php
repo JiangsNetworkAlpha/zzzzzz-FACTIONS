9a1b40fd13089e7b4670fa70dbd49bca6c8f0931
markkrueg <mkrueger@cthub.com>
2015-06-21 13:38:57 -0700

    fixes issue #52
    fixes issue #52 "/f demote xxxx when xxxx is offline crashes server"

--- src/FactionsPro/FactionCommands.php
+++ src/FactionsPro/FactionCommands.php
@@ -227,7 +227,7 @@ class FactionCommands {
 							$sender->sendMessage($this->plugin->formatMessage("Player is not in this faction!"));
 							return true;
 						}
-						if(!$this->plugin->isOfficer($this->plugin->getServer()->getPlayer($args[1])->getName())) {
+						if(!$this->plugin->isOfficer($args[1])) {
 							$sender->sendMessage($this->plugin->formatMessage("Player is already Member"));
 							return true;
 						}
@@ -237,14 +237,14 @@ class FactionCommands {
 						$stmt->bindValue(":faction", $factionName);
 						$stmt->bindValue(":rank", "Member");
 						$result = $stmt->execute();
-						$player = $this->plugin->getServer()->getPlayer($args[1]);
-						$sender->sendMessage($this->plugin->formatMessage("" . $player->getName() . " has been demoted to Member.", true));
+						$player = $args[1];
+						$sender->sendMessage($this->plugin->formatMessage("" . $player . " has been demoted to Member.", true));
 						
 						if($player = $this->plugin->getServer()->getPlayer($args[1])) {
 							$player->sendMessage($this->plugin->formatMessage("You were demoted to Member.", true));
-							if($this->plugin->prefs->get("FactionNametags")) {
-								$this->plugin->updateTag($player->getName());
-							}
+						}
+						if($this->plugin->prefs->get("FactionNametags")) {
+							$this->plugin->updateTag($player->getName());
 						}
 					}
 					
